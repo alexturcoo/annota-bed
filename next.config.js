dc527bc9d7a009b99578d4469e252bb9eef37591
alexturcoo <alexanderturco1@gmail.com>
2025-10-02 22:20:58 -0400

    annota-bed v2 updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const isDev = process.env.NODE_ENV !== 'production';
+const FLASK_URL = process.env.FLASK_URL; // set in Vercel env vars
+
+module.exports = {
   async rewrites() {
-    return [
-      { source: "/api/:path*", destination: "http://127.0.0.1:5328/:path*" },
-    ];
+    if (isDev) {
+      return [{ source: '/api/:path*', destination: 'http://127.0.0.1:5328/:path*' }];
+    }
+    return [{ source: '/api/:path*', destination: `${FLASK_URL}/:path*` }];
   },
 };
-module.exports = nextConfig;
